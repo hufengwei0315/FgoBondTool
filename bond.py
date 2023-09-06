c882b8a18fa2b5fcf8500756e780673283c3b3ef
hufengwei0315 <100832654+hufengwei0315@users.noreply.github.com>
2023-09-06 15:28:41 +0800

    Add files via upload
    生成新文件 羁绊等级文件
    可以先按照羁绊等级分组 后按照羁绊值排序

--- bond.py
+++ bond.py
@@ -2,6 +2,7 @@
 import json
 import os
 import xlsxwriter as xw
+from itertools import groupby
 
 class SvtData:
     def __init__(self,id,svtId,mcLink,bondPoint,bondLevel,bondLeft):
@@ -18,6 +19,17 @@ def sortData(list):
         svtList.append(SvtData(list[j]["id"], list[j]["svtId"], list[j]["mcLink"],list[j]["bondPoint"],list[j]["bondLevel"],list[j]["bondLeft"]))
     svtList.sort(key=lambda x: x.bondLeft)
     return svtList;
+
+def sortDatalv(list):
+    svtList = []
+    for j in range(len(list)):
+        svtList.append(SvtData(list[j]["id"], list[j]["svtId"], list[j]["mcLink"],list[j]["bondPoint"],list[j]["bondLevel"],list[j]["bondLeft"]))
+    svtList.sort(key=lambda x: x.bondLevel)
+    return svtList;
+
+def groupData(dataList):
+    ltsg = groupby(dataList,key= lambda x:x.bondLevel)
+    return ltsg
 
 
 def data_open_excel(list0,list1,list2,list3,list4,list5):
@@ -99,9 +111,87 @@ def data_open_excel(list0,list1,list2,list3,list4,list5):
     workbook.close()
     print('羁绊文件已生成')    
 
+
+def write_data_to_excel(worksheet,svtList,i):
+    dataList = [];
+    for j in range(len(svtList)):
+        #print(svtList[j].id)
+        tinydict ={"id":svtList[j].id,"svtId": svtList[j].svtId,"mcLink":svtList[j].mcLink,"bondPoint":svtList[j].bondPoint,"bondLevel":svtList[j].bondLevel,"bondLeft":svtList[j].bondLeft}
+        dataList.append(tinydict)
+
+    svtListAfter = sortData(dataList)
+
+    for j in range(len(svtListAfter)):
+       insertData = [svtListAfter[j].id, svtListAfter[j].svtId, svtListAfter[j].mcLink,svtListAfter[j].bondPoint,svtListAfter[j].bondLevel,svtListAfter[j].bondLeft]
+       #print(insertData)
+       row = 'A' + str(i)
+       worksheet.write_row(row, insertData)
+       i += 1
+    return i
+
+def writeGroup(groupLevel,worksheet):
+    i = 2  # 从第二行开始写入数据
+    for key,group in groupLevel:
+       insertData = ["羁绊等级"+str(key)]
+       row = 'A' + str(i)
+       worksheet.write_row(row, insertData)
+       i +=1
+       i = write_data_to_excel(worksheet,list(group),i)
+
+def data_open_excel_lv(list0,list1,list2,list3,list4,list5):
+    title = ['从者id', '从者编号', '名称','羁绊点','羁绊等级','剩余羁绊点']  # 设置表头
+    workbook = xw.Workbook('羁绊等级文件.xlsx') 
+    worksheet0 = workbook.add_worksheet("Cost0")
+    worksheet1 = workbook.add_worksheet("Cost1")
+    worksheet2 = workbook.add_worksheet('Cost2')
+    worksheet3 = workbook.add_worksheet('Cost3')
+    worksheet4 = workbook.add_worksheet('Cost4')
+    worksheet5 = workbook.add_worksheet("Cost5")
+    worksheet5.activate()
+    worksheet0.write_row('A1', title)  # 从A1单元格开始写入表头
+    worksheet1.write_row('A1', title)  # 从A1单元格开始写入表头
+    worksheet2.write_row('A1', title)  # 从A1单元格开始写入表头
+    worksheet3.write_row('A1', title)  # 从A1单元格开始写入表头
+    worksheet4.write_row('A1', title)  # 从A1单元格开始写入表头
+    worksheet5.write_row('A1', title)  # 从A1单元格开始写入表头        
+    # 五星 cost 16
+    svtList = sortDatalv(list5)
+    groupLevel = groupData(svtList) 
+    writeGroup(groupLevel,worksheet5)
+    # 四星 cost 12    
+    svtList = sortDatalv(list4)
+    groupLevel = groupData(svtList) 
+    writeGroup(groupLevel,worksheet4)
+
+    # 三星 cost 7
+    svtList = sortDatalv(list3)
+    groupLevel = groupData(svtList) 
+    writeGroup(groupLevel,worksheet3)
+
+    # 二星 cost 4
+    svtList = sortDatalv(list2)
+    groupLevel = groupData(svtList) 
+    writeGroup(groupLevel,worksheet2)
+
+    # 一星 cost 3
+    svtList = sortDatalv(list1)    
+    groupLevel = groupData(svtList) 
+    writeGroup(groupLevel,worksheet1)
+
+    #cost 0
+    svtList = sortDatalv(list0)
+    groupLevel = groupData(svtList) 
+    writeGroup(groupLevel,worksheet0)
+
+    workbook.close()
+    print('羁绊文件已生成')    
+
 if(os.path.exists("羁绊文件.xlsx")):
-    print('过期文件已删除')
+    print('羁绊过期文件已删除')
     os.remove("羁绊文件.xlsx")
+if(os.path.exists("羁绊等级文件.xlsx")):
+    print('羁绊等级过期文件已删除')
+    os.remove("羁绊等级文件.xlsx")
 
 file_name  = "bond_servant.json"
 object = open(file_name,encoding="utf-8")
@@ -176,4 +266,5 @@ for key in range (len(collection)):
 
 
 
-data_open_excel(list0,list1,list2,list3,list4,list5)           
+data_open_excel(list0,list1,list2,list3,list4,list5)
+data_open_excel_lv(list0,list1,list2,list3,list4,list5) 
