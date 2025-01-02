00b87739fdd68aa11a5d1e75746710ada3d83616
hufengwei0315 <100832654+hufengwei0315@users.noreply.github.com>
2025-01-02 16:29:58 +0800

    Add files via upload

--- item_collect.py
+++ item_collect.py
@@ -3,6 +3,10 @@ import json
 import os
 import xlsxwriter as xw
 from itertools import groupby
+import requests
+from openpyxl import Workbook
+from openpyxl.drawing.image import Image
+from io import BytesIO
 
 class SvtData:
     def __init__(self,id,svtId,mcLink,bondPoint,bondLevel,bondLeft,treasureDeviceLv1):
@@ -13,21 +17,25 @@ class SvtData:
         self.bondLevel= bondLevel
         self.bondLeft = bondLeft
         self.treasureDeviceLv1 = treasureDeviceLv1
-class SvtData:
-    def __init__(self,id,svtId,mcLink,appendFlag,skillLv):
-        self.id = id
-        self.svtId = svtId
-        self.mcLink = mcLink
-        self.appendFlag = appendFlag
-        self.skillLv= skillLv
+class itemData:
+    def __init__(self,itemName,itemId,count,url):
+        self.itemName = itemName
+        self.itemId = itemId
+        self.count= count
+        self.url = url
+
+def sortData(list):
+    svtList = []
+    for j in range(len(list)):
+        svtList.append(itemData(list[j]["itemName"], list[j]["itemId"], list[j]["count"],list[j]["url"]))
+    svtList.sort(key=lambda x: x.count)
+    return svtList;
 
 
-
-
-
+os.environ["http_proxy"] = "http://127.0.0.1:7991"
+os.environ["https_proxy"] = "http://127.0.0.1:7991"
 file_name  = "bond_servant.json"
 object = open(file_name,encoding="utf-8")
-
 try:
     all_the_text = object.read()  #结果为str类型
     svt_data = json.loads(all_the_text);
@@ -36,26 +44,33 @@ finally:
 
 user_data_path = "result.json" 
 user_file_object = open(user_data_path,encoding="utf-8")
-
 try:
     user_data_text = user_file_object.read()  #结果为str类型
 finally:
-    object.close()
+    user_file_object.close()
 
 servant_data_path = "base/servants.json"
 servant_data_object = open(servant_data_path,encoding="utf-8")
-
 try:
     servant_data_text = servant_data_object.read()  #结果为str类型
 finally:
-    object.close()
+    servant_data_object.close()
+
+
+item_data_path="bond_item.json"
+item_data_object = open(item_data_path,encoding="utf-8")
+try:
+    item_data_text = item_data_object.read()  #结果为str类型
+finally:
+    item_data_object.close()
+
 
 user_dic = json.loads(user_data_text)
 collection = user_dic['cache']['replaced']['userSvt']
 #print(collection)
 #print (type(collection))
 servant_dic = json.loads(servant_data_text)
-
+item_idc= json.loads(item_data_text)
 
 list0 = [];
 
@@ -63,8 +78,64 @@ collection2 = user_dic['cache']['replaced']['userSvtAppendPassiveSkill']
 #print(len(collection2))
 
 collection3 = user_dic['cache']['replaced']['userSvtAppendPassiveSkillLv']
+user_item =  user_dic['cache']['replaced']['userItem']
 #print(len(collection3))
 
+
+def download_image(url):
+    """下载图片并返回图片的二进制数据"""
+    response = requests.get(url)
+    response.raise_for_status()  # 检查请求是否成功
+    return BytesIO(response.content)
+def insert_excel(list0):
+    if(os.path.exists("素材详情.xlsx")):
+        print('素材详情已删除')
+        os.remove("素材详情.xlsx")
+
+    title = ['id', '名称', '素材','数量','素材id']  # 设置表头
+    workbook = xw.Workbook('素材详情.xlsx') 
+    worksheet0 = workbook.add_worksheet("sheet1")
+    worksheet0.write_row('A1', title)  # 从A1单元格开始写入表头
+    i = 2  # 从第二行开始写入数据
+    for j in range(len(list0)):
+        #print(list1[j])
+        insertData = [list0[j]['svtId'], list0[j]['mclink'], list0[j]['itemName'],list0[j]['count'],list0[j]['itemId']]
+        #print(insertData)
+        row = 'A' + str(i)
+        worksheet0.write_row(row, insertData)
+        i += 1
+    workbook.close()
+
+def insert_total_excel(list0):
+    if(os.path.exists("素材总需求.xlsx")):
+        print('素材总需求已删除')
+        os.remove("素材总需求.xlsx")
+
+    title = ['图片','素材','数量','素材id']  # 设置表头
+    workbook = xw.Workbook('素材总需求.xlsx') 
+    worksheet0 = workbook.add_worksheet("sheet1")
+    worksheet0.write_row('A1', title)  # 从A1单元格开始写入表头
+    i = 2  # 从第二行开始写入数据
+    itemList = sortData(list0)
+    for j in range(len(itemList)):
+        worksheet0.set_row(i-1,30)
+        #print(list1[j])
+        try:
+            print("正在下载...."+itemList[j].url)
+            image_data = download_image(itemList[j].url)
+            print("下载完成！ "+itemList[j].itemName)
+            worksheet0.insert_image(i-1,0,f"image_{j+1}.png",
+                                    {"image_data":image_data,
+                                     "x_scale": 0.25,
+                                    "y_scale": 0.25})
+        except Exception as e:
+            worksheet0.write(i, 4, f"Error: {e}") 
+        insertData = [ itemList[j].itemName,itemList[j].count,itemList[j].itemId]
+        #print(insertData)
+        row = 'B' + str(i)
+        worksheet0.write_row(row, insertData)    
+        i += 1
+    workbook.close()
 
 for key in range (len(collection)):
     id = int(collection[key]['id'])
@@ -81,8 +152,6 @@ for key in range (len(collection)):
         if((int)(userSvtId) == id):
             appendPassiveSkillLvs = collection3[key2]['appendPassiveSkillLvs']
             appendPassiveSkillNums = collection3[key2]['appendPassiveSkillNums']
-            #print(appendPassiveSkillLvs)
-            #print(appendPassiveSkillNums)
             for num in range(len(appendPassiveSkillNums)):
                 sequence = appendPassiveSkillNums[num]
                 if sequence == 101:
@@ -111,34 +180,72 @@ for key in range (len(collection)):
         tinydict ={"id":idX,"svtId":svt_data[n]['collectionNo'],"mcLink":svt_data[n]['mcLink'],"appendFlag":True,"skillLv":appendSkillLv5}
         list0.append(tinydict)
 #print(list0)
+result = []
 for index in range(len(list0)):
     svtId = list0[index]['id']
     level =(int) (list0[index]['skillLv'])
     appendFlag = list0[index]['appendFlag']
+    
     #print(svtId)
     for key2 in range(len (servant_dic)):
         id = servant_dic[key2]['id']
-        #print(id+" "+svtId)
-        #print(id)
         #input("Press Enter to continue...")
         if (id == svtId ):
-            #print(int(id)+" "+svtId)
             #input("Press Enter to continue...")
-            if( appendFlag == True):
+            if( appendFlag == False):
                 skillMaterials = servant_dic[key2]['skillMaterials']
             else:
-                 skillMaterials = servant_dic[key2]['appendSkillMaterials']
+                skillMaterials = servant_dic[key2]['appendSkillMaterials']
          
             if level != 10:
-                for tempLevel in range (len(skillMaterials)-1):
-                    #print(skillMaterials['1'])
-                    #print(tempLevel)
-                    #print(level)
+                for tempLevel in range (len(skillMaterials)):
                     if tempLevel+1 > level :
                         levelItemList = skillMaterials[str(tempLevel)]
-                        print(levelItemList)
                         for j in range(len(levelItemList['items'])):
                             item = levelItemList['items'][j]
-                            print(str(svtId)+" "+str(item['itemId'])+" "+str(item['amount']))
-                            #print(str(item['amount']))
+                            itemId = str(item['itemId'])
+                            count = int(item['amount'])
+                            for k in range(len(item_idc)) :
+                                if int(item_idc[k]['id']) == int(itemId):
+                                    #print(str(svtId)+" "+list0[index]['mcLink']+" "+str(item_idc[k]['name'])+" "+str(item['amount']))
+                                    result_dict = {"svtId":svtId,'mclink':list0[index]['mcLink'],"level":tempLevel,"itemName":str(item_idc[k]['name']),"itemId":itemId,"count":count,"url":item_idc[k]['icon']}  
+                                    result.append(result_dict)
+
+#print(result)
+
+insert_excel(result)
+
+total_result = []
+for j in range(len(result)):
+    itemId= int(result[j]['itemId'])
+    count = int(result[j]['count'])
+    itemName = result[j]['itemName']
+    url = result[j]['url']
+    exist_flag = False
+    k = -1
+    for index in range(len(total_result)):
+        if total_result[index]['itemId'] == itemId:
+            exist_flag = True
+            k = index
+    
+    if exist_flag:
+        total_amount = int(total_result[k]['count']) + int (result[j]['count'])
+        total_result[k]['count'] = total_amount
+    else :
+        result_dict = {"itemName":itemName,"itemId":itemId,"count":count,"url":url}  
+        total_result.append(result_dict)
+for j in range(len(total_result)):
+    result_item  = total_result[j]
+    for index in range(len(user_item)):
+        item_id = user_item[index]['itemId']
+        if int(item_id) == int(result_item['itemId']):
+            total_amount =    int (user_item[index]['num']) - int(total_result[j]['count'])
+            total_result[j]['count'] = total_amount
+
+insert_total_excel(total_result)
+
+
+                                
+
+                            
                     
